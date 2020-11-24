14699da18b1210390c99aa479e04bb18a4a9d3ae
unknown <sahanbcsrh@gmail.com>
2020-11-24 15:29:44 +0530

    verifivation list

--- api/divisional-office/divisional-office.routers.js
+++ api/divisional-office/divisional-office.routers.js
@@ -5,7 +5,7 @@ const {
   createDivisionalOffice,
   updateDivisionalOffice,
   deleteDivisionalOffice,
-  getToBeVerifyList,
+
 } = require("./divisional-office.conrollers");
 const { checkToken } = require("../../auth/token_validation");
 
@@ -13,7 +13,6 @@ router.post("/", checkToken, createDivisionalOffice);
 
 router.get("/", checkToken, getDivisionalOffices);
 
-router.get("/verifylist", checkToken, getToBeVerifyList);
 router.get("/:div_off_id", checkToken, getDivisionalOfficeByID);
 router.patch("/", checkToken, updateDivisionalOffice);
 router.delete("/", checkToken, deleteDivisionalOffice);
