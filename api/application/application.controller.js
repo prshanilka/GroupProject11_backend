8ad54853a3507fef8c2afbc580428604073fbd78
prshanilka <ramesh.shanilka@gmail.com>
2020-11-24 18:30:12 +0530

    asdasd

---
+++ api/application/application.controller.js
@@ -0,0 +1,27 @@
+const {
+  getApplicationStatus
+} = require("./application.service");
+const { checkPermision } = require("../../auth/roleauth");
+
+module.exports = {
+  getApplicationStatus: (req, res) => {
+    const elder_id = req.auth.result.user_id;
+    getApplicationStatus(elder_id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record not found",
+        });
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+
+};
