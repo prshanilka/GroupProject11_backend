c501fcffc6934fd607dd464d03d8c25e8a7cd01c
gmgpathum@gmail.com <gmgpathum@gmail.com>
2020-11-25 14:17:00 +0530

    Div Officer Validity

--- api/verify_elder/verify_elder.router.js
+++ api/verify_elder/verify_elder.router.js
@@ -8,6 +8,9 @@ const {
   deleteverifyElder,
   updateVerifyElderAfterGramAccept,
   updateVerifyElderAfterGramDisQualified,
+  updateVerifyElderAfterDiv_Off_Accept,
+  updateVerifyElderAfterDiv_Off_DisQualified
+
 } = require("./verify_elder.controller");
 
 const { checkToken } = require("../../auth/token_validation");
@@ -25,5 +28,7 @@ router.patch(
 
 router.patch("/", checkToken, updateverifyElder);
 router.delete("/", checkToken, deleteverifyElder);
+router.patch("/divoffaccept",checkToken,updateVerifyElderAfterDiv_Off_Accept);
+router.patch("/divofffdisqulify",checkToken,updateVerifyElderAfterDiv_Off_DisQualified);
 
 module.exports = router;
