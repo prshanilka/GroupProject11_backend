45cb85308938ea63b862ae06da9a8df98d75fdc5
gmgpathum@gmail.com <gmgpathum@gmail.com>
2020-11-29 01:08:56 +0530

    Grama Niladari Details

--- api/grama_niladari_officer/officer_service.js
+++ api/grama_niladari_officer/officer_service.js
@@ -74,7 +74,21 @@ module.exports = {
 
     GetGramaOfficerByOfficers: (data, callBack) => {
       pool.query(
-        "SELECT * FROM gramaniladari,officers WHERE gramaniladari.grmaniladari_officer_id = officers.officer_id",
+        "SELECT officers.name AS oname,gramaniladari_division.name AS dname,officers.email,officers.phone,gramaniladari.grmaniladari_officer_id FROM officers,gramaniladari,gramaniladari_division WHERE officers.officer_id = gramaniladari.grmaniladari_officer_id AND gramaniladari.gramaniladari_division_id = gramaniladari_division.gramaniladari_division_id",
+        [],
+        (error, results, fields) => {
+          if (error) {
+            console.log(results);
+            return callBack(error);
+          }
+          return callBack(null, results);
+        }
+      );
+    },
+    
+    GetGramaOfficerByOfficers: (data, callBack) => {
+      pool.query(
+        "SELECT officers.name AS oname,gramaniladari_division.name AS dname,officers.email,officers.phone,gramaniladari.grmaniladari_officer_id,gramaniladari.gramaniladari_division_id FROM officers,gramaniladari,gramaniladari_division WHERE officers.officer_id = gramaniladari.grmaniladari_officer_id AND gramaniladari.gramaniladari_division_id = gramaniladari_division.gramaniladari_division_id",
         [],
         (error, results, fields) => {
           if (error) {
@@ -86,4 +100,19 @@ module.exports = {
       );
     },
 
+    GetGramaDetails: (division_id, callBack) => {
+      pool.query(
+        "SELECT officers.name AS oname,gramaniladari_division.name AS dname,officers.email AS oemail,officers.phone AS ophone ,officers.nic_no,gramaniladari.grmaniladari_officer_id,gramaniladari.gramaniladari_division_id,gramaniladari.district_id,gramaniladari.divisional_secratary_id,gramaniladari_division.address,gramaniladari_division.number,gramaniladari_division.email AS demail,gramaniladari_division.count_of_benifishers FROM officers,gramaniladari,gramaniladari_division WHERE officers.officer_id = gramaniladari.grmaniladari_officer_id AND gramaniladari.gramaniladari_division_id = gramaniladari_division.gramaniladari_division_id AND gramaniladari_division.gramaniladari_division_id = ?",
+        [
+          division_id
+        ],
+        (error, results, fields) => {
+          if (error) {
+            console.log(results);
+            return callBack(error);
+          }
+          return callBack(null, results[0]);
+        }
+      );
+    }
 };
