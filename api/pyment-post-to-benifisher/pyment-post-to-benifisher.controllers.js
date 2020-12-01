f3ba1f40576102ef88a64605b984ed991c6b4544
unknown <sahanbcsrh@gmail.com>
2020-12-01 14:11:39 +0530

    all pay detail

--- api/pyment-post-to-benifisher/pyment-post-to-benifisher.controllers.js
+++ api/pyment-post-to-benifisher/pyment-post-to-benifisher.controllers.js
@@ -6,6 +6,7 @@ const {
   payToAgent,
   getElderHistory,
   updateElderReason,
+  getAllPayReport,
 } = require("./pyment-post-to-benifisher.services");
 const { checkPermision } = require("../../auth/roleauth");
 
@@ -67,6 +68,35 @@ module.exports = {
       year: req.params.year,
     };
     getBenifisherPayemtListByPostOffice(data, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          succcess: 0,
+          message: "Database Connection error",
+        });
+      }
+
+      return res.status(200).json({
+        success: 1,
+        status: true,
+        total: 5,
+        last_page: 1,
+        per_page: 8,
+        current_page: 1,
+        next_page_url:
+          "https://api.coloredstrategies.com/cakes/fordatatable?sort=&page=2&per_page=8",
+        prev_page_url:
+          "https://api.coloredstrategies.com/cakes/fordatatable?sort=&page=2&per_page=8",
+        from: 1,
+        to: 8,
+        data: result,
+      });
+    });
+  },
+  getAllPayReport: (req, res) => {
+    const div_id = req.params.div_id;
+
+    getAllPayReport(div_id, (err, result) => {
       if (err) {
         console.log(err);
         return res.status(500).json({
--- api/pyment-post-to-benifisher/pyment-post-to-benifisher.routers.js
+++ api/pyment-post-to-benifisher/pyment-post-to-benifisher.routers.js
@@ -7,6 +7,7 @@ const {
   payToAgent,
   getElderHistory,
   updateElderReason,
+  getAllPayReport,
 } = require("./pyment-post-to-benifisher.controllers");
 const { checkToken } = require("../../auth/token_validation");
 
@@ -20,6 +21,7 @@ router.get(
   checkToken,
   getBenifisherPayemtListByPostOffice
 );
+router.get("/allpayreport/:div_id", checkToken, getAllPayReport);
 router.get("/elderhistory/:eld_id", checkToken, getElderHistory);
 router.get("/", checkToken, getBenifisherPayemtList);
 
@@ -27,4 +29,5 @@ router.patch("/elderreason", checkToken, updateElderReason);
 router.post("/paytoelder", checkToken, payToElder);
 
 router.post("/paytoagent", checkToken, payToAgent);
+
 module.exports = router;
