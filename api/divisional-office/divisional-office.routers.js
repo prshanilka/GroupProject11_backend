93e4df01864f0f09dc6ee96b3dd08a074b80d505
gmgpathum@gmail.com <gmgpathum@gmail.com>
2020-12-06 19:15:51 +0530

    Constant

--- api/divisional-office/divisional-office.routers.js
+++ api/divisional-office/divisional-office.routers.js
@@ -8,18 +8,23 @@ const {
   getApplicationToVerifyByDivision,
   getDivisionsToSelectBox,
   getBenifisherListTodiv,
+  getConstant,
+  updateConstant
 } = require("./divisional-office.conrollers");
 const { checkToken } = require("../../auth/token_validation");
 
 router.get("/selectbox", checkToken, getDivisionsToSelectBox);
 router.post("/", checkToken, createDivisionalOffice);
-
+router.get("/cons", checkToken, getConstant);
 router.get("/", checkToken, getDivisionalOffices);
 
 router.get("/benifisherlist/:off_id", checkToken, getBenifisherListTodiv);
 router.get("/list/:div_off_id", checkToken, getApplicationToVerifyByDivision);
 router.get("/:div_off_id", checkToken, getDivisionalOfficeByID);
+
 router.patch("/", checkToken, updateDivisionalOffice);
+router.patch("/cons", checkToken, updateConstant);
+
 router.delete("/", checkToken, deleteDivisionalOffice);
 
 module.exports = router;
